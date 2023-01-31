a74990d632fe32194ab6c536e1bd2de9d902d520
DaShaun <dashaun@dashaun.com>
2023-01-30 23:00:04 -0600

    update the prompt, add image, some docs

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # forefront-ai-client
 
+![client details](docs/forefront-ai-client.png)
+
 [ForeFront](https://www.forefront.ai/) has a "summarize" API, that allows me to send in larger blocks of text, than I can with the currently popular ChatGPT.
 
 This client asks for an input file, compression level, and uses the ForeFront summarize API.
@@ -8,3 +10,10 @@ This client asks for an input file, compression level, and uses the ForeFront su
 
 - `forefront.teamId` is the short-form of the team id
 - `forefront.modelKey` is found in settings -> API Keys -> Model Keys
+
+## Quick Start
+
+```java
+./mvnw spring-boot:run
+```
+
--- src/main/java/dev/dashaun/client/ai/forefront/cli/Application.java
+++ src/main/java/dev/dashaun/client/ai/forefront/cli/Application.java
@@ -1,8 +1,12 @@
 package dev.dashaun.client.ai.forefront.cli;
 
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.shell.jline.PromptProvider;
+import org.springframework.stereotype.Component;
 
 @EnableConfigurationProperties(ConfigProps.class)
 @SpringBootApplication
@@ -10,6 +14,17 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+}
+
+@Component
+class CustomPromptProvider implements PromptProvider {
+
+	@Override
+	public AttributedString getPrompt() {
+		return new AttributedString("forefront.ai:>",
+				AttributedStyle.DEFAULT.foreground(AttributedStyle.GREEN));
 	}
 
 }
